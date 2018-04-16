a2adf05f15c2be279b6d34df43a15e2c526f89ec
USUARIO <USUARIO@JmartinNavas>
2018-04-15 20:56:07 -0500

    refactorizacion extract method en la clase AgregarProducto

--- src/java/Servlets/Ventas.java
+++ src/java/Servlets/Ventas.java
@@ -8,7 +8,6 @@ package Servlets;
 import DAOs.DAOArticulo;
 import DAOs.DAOVentas;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.LinkedList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -24,41 +23,38 @@ import modelos.ArticuloCarrito;
  */
 public class Ventas extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+    /** Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
-     *
-     * @param request servlet request
+       @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    protected void processRequest(final HttpServletRequest request, final HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        DAOArticulo daoart = new DAOArticulo();
-        DAOVentas daoven = new DAOVentas();
+        final DAOArticulo daoarticulo = new DAOArticulo();
+        final DAOVentas daoventas = new DAOVentas();
         int total = 0;
-        HttpSession sesion = request.getSession(true);
-        LinkedList<ArticuloCarrito> articulosCarrito = sesion.getAttribute("carrito") == null ? null : (LinkedList<ArticuloCarrito>) sesion.getAttribute("carrito");
+        final HttpSession sesion = request.getSession(true);
+        final LinkedList<ArticuloCarrito> articulosCarrito = sesion.getAttribute("carrito") == null ? null : (LinkedList<ArticuloCarrito>) sesion.getAttribute("carrito");
         if (articulosCarrito != null) {
-            for (ArticuloCarrito ac : articulosCarrito) {
-                Articulo ar = daoart.seleccionarArticulo(ac.getIdArticulo());
-                total = total + (ac.getCantidad() * ar.getPrecio());
+            for (final ArticuloCarrito ac : articulosCarrito) {
+                final Articulo articulo = daoarticulo.seleccionarArticulo(ac.getIdArticulo());
+                total = total + (ac.getCantidad() * articulo.getPrecio());
             }
 
-            daoven.guardarVenta(1, total);
+            daoventas.guardarVenta(1, total);
 
-            for (ArticuloCarrito ac : articulosCarrito) {
-                Articulo ar = daoart.seleccionarArticulo(ac.getIdArticulo());
-                daoven.guardarVentaCantidad(daoven.selectMaxIdVentas(), ac.getIdArticulo(), ac.getCantidad());
+            for (final ArticuloCarrito ac : articulosCarrito) {
+                daoventas.guardarVentaCantidad(daoventas.selectMaxIdVentas(), ac.getIdArticulo(), ac.getCantidad());
             }
             articulosCarrito.removeAll(articulosCarrito);
         }
         response.sendRedirect("index.htm");
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
@@ -68,7 +64,7 @@ public class Ventas extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    protected void doGet(final HttpServletRequest request,final HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
@@ -82,7 +78,7 @@ public class Ventas extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    protected void doPost(final HttpServletRequest request,final HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
